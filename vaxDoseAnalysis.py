334283a54b5062a8a0b0804780f5f344c70421d9
Choo-Yee Ting <cyting@mmu.edu.my>
2021-04-15 21:06:14 +0800

    Add files via upload

--- vaxDoseAnalysis.py
+++ vaxDoseAnalysis.py
@@ -51,27 +51,60 @@ variable = df.columns.tolist()
 variable.remove('Date')
 df_long = pd.melt(df, id_vars=['Date'], value_vars= variable)   
 
+col1, col2 = st.beta_columns(2)
 
-state_option = st.selectbox('Select the State',('Malaysia','Johor','Kedah','Kelantan','Melaka','NS','Pahang','Penang','Perak','Perlis','Sarawak','Sabah','Selangor','Terengganu','KL','Putrajaya','Labuan'))
+with col1:
+    state_option = st.selectbox('Select the State',('Malaysia','Johor','Kedah','Kelantan','Melaka','NS','Pahang','Penang','Perak','Perlis','Sarawak','Sabah','Selangor','Terengganu','KL','Putrajaya','Labuan'))
 
+    state_1st_dose_daily = state_option + ' 1st dose daily'
+    state_2nd_dose_daily = state_option + ' 2nd dose daily'
 
-state_1st_dose_daily = state_option + ' 1st dose daily'
-state_2nd_dose_daily = state_option + ' 2nd dose daily'
+    state_1st_dose = state_option + ' 1st dose'
+    state_2nd_dose = state_option + ' 2nd dose'
 
-state_1st_dose = state_option + ' 1st dose'
-state_2nd_dose = state_option + ' 2nd dose'
+    state_1st_dose_daily_7d_avg = state_option + ' 1st dose daily 7d average'
+    state_2nd_dose_daily_7d_avg = state_option + ' 2nd dose daily 7d average'
 
-state_1st_dose_daily_7d_avg = state_option + ' 1st dose daily 7d average'
-state_2nd_dose_daily_7d_avg = state_option + ' 2nd dose daily 7d average'
+    state_1st_dose_daily_7d_avg_100k = state_option + ' 1st dose daily 7d average per 100k'
+    state_2nd_dose_daily_7d_avg_100k = state_option + ' 2nd dose daily 7d average per 100k'
 
-state_1st_dose_daily_7d_avg_100k = state_option + ' 1st dose daily 7d average per 100k'
-state_2nd_dose_daily_7d_avg_100k = state_option + ' 2nd dose daily 7d average per 100k'
+    filter_list = [state_1st_dose_daily, state_2nd_dose_daily, state_1st_dose, state_2nd_dose, state_1st_dose_daily_7d_avg, state_2nd_dose_daily_7d_avg, state_1st_dose_daily_7d_avg_100k, state_2nd_dose_daily_7d_avg_100k]
+    results = df_long[df_long.variable.isin(filter_list)] 
+    fig = px.line(results, x="Date", y="value", color="variable", title='1st and 2nd Dose Information', 
+                labels=dict(value="Value"), template="none",
+                width=1200, height=700)
+    fig.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01))
 
-filter_list = [state_1st_dose_daily, state_2nd_dose_daily, state_1st_dose, state_2nd_dose, state_1st_dose_daily_7d_avg, state_2nd_dose_daily_7d_avg, state_1st_dose_daily_7d_avg_100k, state_2nd_dose_daily_7d_avg_100k]
-results = df_long[df_long.variable.isin(filter_list)] 
-fig = px.line(results, x="Date", y="value", color="variable", title='1st and 2nd Dose Information', 
-            labels=dict(value="Value"), template="none",
-            width=1200, height=700)
+    st.plotly_chart(fig,use_container_width=True)
 
-st.plotly_chart(fig,use_container_width=True)
+with col2: 
+    state_option2 = st.selectbox('Select the State',('Malaysia','Johor','Kedah','Kelantan','Melaka','NS','Pahang','Penang','Perak','Perlis','Sarawak','Sabah','Selangor','Terengganu','KL','Putrajaya','Labuan'), key = "new")
+
+    state_1st_dose_daily = state_option2 + ' 1st dose daily'
+    state_2nd_dose_daily = state_option2 + ' 2nd dose daily'
+
+    state_1st_dose = state_option2 + ' 1st dose'
+    state_2nd_dose = state_option2 + ' 2nd dose'
+
+    state_1st_dose_daily_7d_avg = state_option2 + ' 1st dose daily 7d average'
+    state_2nd_dose_daily_7d_avg = state_option2 + ' 2nd dose daily 7d average'
+
+    state_1st_dose_daily_7d_avg_100k = state_option2 + ' 1st dose daily 7d average per 100k'
+    state_2nd_dose_daily_7d_avg_100k = state_option2 + ' 2nd dose daily 7d average per 100k'
+
+    filter_list = [state_1st_dose_daily, state_2nd_dose_daily, state_1st_dose, state_2nd_dose, state_1st_dose_daily_7d_avg, state_2nd_dose_daily_7d_avg, state_1st_dose_daily_7d_avg_100k, state_2nd_dose_daily_7d_avg_100k]
+    results = df_long[df_long.variable.isin(filter_list)] 
+    fig2 = px.line(results, x="Date", y="value", color="variable", title='1st and 2nd Dose Information', 
+                labels=dict(value="Value"), template="none",
+                width=1200, height=700)
+    fig2.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01))
+    st.plotly_chart(fig2,use_container_width=True)
 
